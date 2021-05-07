8a9a9ae2cc32d2c6c53ba4c58b1fb04ce628e74a
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-05-07 09:16:59 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
+
+app = Flask(__name_)
+
+@app.route('/')
+def
+
+@app.route('/posts')
+def
+
+@app.route('/login')
+def
+
+@app.route('/logout')
+def
