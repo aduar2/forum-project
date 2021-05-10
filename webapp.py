7826db8b246219b6bc3eeb3bbd32df0a2e624c74
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-05-10 09:56:43 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,9 +1,45 @@
-import os
-from flask import Flask, url_for, render_template, request
-from flask import redirect
-from flask import session
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+#from flask_oauthlib.contrib.apps import github #import to make requests to GitHub's OAuth
+from flask import render_template
 
-app = Flask(__name_)
+import pprint
+import os
+
+# This code originally from https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
+# Edited by P. Conrad for SPIS 2016 to add getting Client Id and Secret from
+# environment variables, so that this will work on Heroku.
+# Edited by S. Adams for Designing Software for the Web to add comments and remove flash messaging
+
+app = Flask(__name__)
+
+app.debug = False #Change this to False for production
+#os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #Remove once done debugging
+
+app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
+oauth = OAuth(app)
+oauth.init_app(app) #initialize the app to be able to make requests for user information
+
+#Set up GitHub as OAuth provider
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
+    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',  
+    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+)
+
+
+#context processors run before templates are rendered and add variable(s) to the template's context
+#context processors must return a dictionary 
+#this context processor adds the variable logged_in to the conext for all templates
+@app.context_processor
+def inject_logged_in():
+    return {"logged_in":('github_token' in session)}
 
 @app.route('/')
 def home ()
