867c2fe5d155f0d25e93a97ca11f3a6911627670
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-05-13 09:58:44 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,6 +6,9 @@ from flask import render_template
 import pprint
 import os
 
+import pymongo
+import sys
+
 app = Flask(__name__)
 
 app.debug = False
@@ -28,6 +31,13 @@ github = oauth.remote_app(
     authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
 )
 
+def main():
+    connection_string = os.environ["MONGO_CONNECTION_STRING"]
+    db_name = os.environ["MONGO_DBNAME"]
+    
+    client = pymongo.MongoClient(connection_string)
+    db = client[db_name]
+    collection = db['forumPosts']
 
 #context processors run before templates are rendered and add variable(s) to the template's context
 #context processors must return a dictionary 
