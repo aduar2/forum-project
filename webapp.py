5d6cf4adbed1748579bc5b0131efe478c7ab8d6e
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-05-14 09:33:45 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -55,7 +55,7 @@ def blog():
 
 @app.route('/login')
 def login():
-  return github.authorize(callback=url_for('authorized', _external=True, _scheme='http'))
+  return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
 
 @app.route('/logout')
 def logout():
